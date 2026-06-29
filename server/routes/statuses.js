f751c09438d38826f577a1ec7a5758bcfa6d7932
FoxxMGR <magomacievgadzimurad@gmail.com>
2026-06-29 15:16:01 +0300

    шаг 9.35

--- server/routes/statuses.js
+++ server/routes/statuses.js
@@ -37,7 +37,7 @@ export default (app) => {
     .post('/statuses/:id', { name: 'updateStatus' }, async (req, reply) => {
       const status = await app.objection.models.taskStatus.query().findById(req.params.id);
       try {
-        await status.$query().patch(req.body.data);
+        await status.$query().patch({ name: req.body.data.name });
         req.flash('info', i18next.t('flash.statuses.update.success'));
         reply.redirect(app.reverse('statuses'));
       } catch (err) {
